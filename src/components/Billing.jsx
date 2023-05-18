36ec16633f4f045a20db25884363e73c4139ea65
quocbaole255 <lenguyenquocbaotqt2016@gmail.com>
2023-05-18 16:36:44 +0700

    first commit

---
+++ src/components/Billing.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Billing = () => {
+  return <div>Billing</div>;
+};
+
+export default Billing;
---
+++ src/components/CTA.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const CTA = () => {
+  return <div>CTA</div>;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default CTA;
